36674e35c359e69dfacbd58113e115ee99656b93
reidhin <hansweda@gmail.com>
2021-09-22 10:57:11 +0200

    using environment variables

--- miljoenen/settings.py
+++ miljoenen/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rbl#nhwwjw8*(oi7g1o^c3f!em*zdhi785s1g0roug@7r&u5&y'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    'django-insecure-rbl#nhwwjw8*(oi7g1o^c3f!em*zdhi785s1g0roug@7r&u5&y'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', 'localhost')]
 
