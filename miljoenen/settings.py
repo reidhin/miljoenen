66ca8a436da3aba506e1345357edbe6a14c93a9a
reidhin <hansweda@gmail.com>
2021-09-22 10:49:17 +0200

    adding allowed hosts

--- miljoenen/settings.py
+++ miljoenen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-rbl#nhwwjw8*(oi7g1o^c3f!em*zdhi785s1g0roug@7r&u5&y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', 'localhost')]
 
 
 # Application definition
